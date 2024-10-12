214e8cf5ec1eb978a519437c554257b996700c81
CJ-Felicitas <cedrick@apollo.com.ph>
2024-10-12 19:45:22 +0800

    first commit

---
+++ src/main/java/com/myralla/loyalty/LoyaltyApplication.java
@@ -0,0 +1,13 @@
+package com.myralla.loyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myralla/loyalty/LoyaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myralla.loyalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
