822412d022ac2806771bec8cf85ce4966037c79a
CJ-Felicitas <cedrick@apollo.com.ph>
2024-11-23 23:22:49 +0800

    feat (registration) : Account and Tenant Registration

--- src/main/java/com/myralla/loyalty/LoyaltyApplication.java
+++ src/main/java/com/myralla/loyalty/LoyaltyApplication.java
@@ -2,10 +2,11 @@ package com.myralla.loyalty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class LoyaltyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoyaltyApplication.class, args);
 	}
---
+++ src/main/java/com/myralla/loyalty/Models/Entity/Tenants.java
@@ -0,0 +1,44 @@
+package com.myralla.loyalty.Models.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tenants")
+@Getter
+@Setter
+public class Tenants {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "org_key", unique = true, nullable = false)
+    private String orgKey;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @Column(name = "updated_at")
+    private String updatedAt;
+
+    // api key
+    @Column(name = "api_key")
+    private String apiKey;
+
+    // api secret
+    @Column(name = "api_secret")
+    private String apiSecret;
+}
